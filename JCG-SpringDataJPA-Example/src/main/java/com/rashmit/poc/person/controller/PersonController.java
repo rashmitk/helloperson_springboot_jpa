ec960deebb2ba81768cc41c1db44c849e894e11b
rashmitk <rashmit.rathod@yahoo.com>
2018-10-28 12:44:12 +0530

    Corrected DB error issue, working fine now

--- JCG-SpringDataJPA-Example/src/main/java/com/rashmit/poc/person/controller/PersonController.java
+++ JCG-SpringDataJPA-Example/src/main/java/com/rashmit/poc/person/controller/PersonController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@EnableAutoConfiguration
 public class PersonController {
 
     private static final Logger LOG = LoggerFactory.getLogger("JCG");
